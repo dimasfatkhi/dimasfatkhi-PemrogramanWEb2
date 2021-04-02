2b85315c1c162b117a0470c37500f9b6d7521f95
dimasfatkhi <80515035+dimasfatkhi@users.noreply.github.com>
2021-04-03 05:44:49 +0700

    Create Pert6_latihan3.php

---
+++ Pert6_latihan3.php
@@ -0,0 +1,11 @@
+<?php
+$UsiaKaryawan["Syarif"] = "21";
+$UsiaKaryawan["Hidayat"] = "24";
+$UsiaKaryawan["Gandhi"] = "45";
+$UsiaKaryawan["Taqim"] = "50";
+$UsiaKaryawan["Sulis"] = "23";
+foreach($UsiaKaryawan as $Nama => $umur)
+{
+echo "Nama Karyawan: $Nama, Usia: $umur"." th <br>";
+}
+?>
